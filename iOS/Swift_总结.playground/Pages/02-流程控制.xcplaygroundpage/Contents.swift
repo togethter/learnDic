20c028c365810c83326f29cbc489423b10dbf8cf
lxl <814508196@qq.com>
2021-03-14 16:25:19 +0800

    swift

---
+++ iOS/Swift_总结.playground/Pages/02-流程控制.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+let age = 4
+if age > 22 {
+    print("get married")
+} else if age > 18 {
+    print("...")
+} else if age > 7 {
+    print("go to school")
+} else {
+    print("just a child")
+}
+
